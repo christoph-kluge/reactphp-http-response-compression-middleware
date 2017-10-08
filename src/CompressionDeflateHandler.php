4d3451251a92dc5140dfe79ad6f7570dad6b131d
Christoph Kluge <work@christoph-kluge.eu>
2017-10-08 10:58:17 +0100

    Handle HttpBodyStream with on-the-fly compression while all other StreamInterfaces will compress the given content

--- src/CompressionDeflateHandler.php
+++ src/CompressionDeflateHandler.php
@@ -5,26 +5,16 @@ namespace Sikei\React\Http\Middleware;
 use Clue\React\Zlib\ZlibFilterStream;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamInterface;
-use React\EventLoop\LoopInterface;
 use React\Http\HttpBodyStream;
-use React\Stream\ReadableResourceStream;
-use React\Stream\ThroughStream;
 
 class CompressionDeflateHandler implements CompressionHandlerInterface
 {
-
-    protected $loop;
-
-    public function __construct(LoopInterface $loop)
-    {
-        $this->loop = $loop;
-    }
 
     public function compressible(ServerRequestInterface $request)
     {
         $accept = $request->getHeaderLine('Accept-Encoding');
 
-        return stristr($accept, 'deflate') !== false;
+        return stristr($accept, $this->__toString()) !== false;
     }
 
     public function __toString()
@@ -38,13 +28,14 @@ class CompressionDeflateHandler implements CompressionHandlerInterface
             return $body;
         }
 
-        $in = new ReadableResourceStream($body->detach(), $this->loop);
-        $out = new ThroughStream();
+        if ($body instanceof HttpBodyStream) {
+            return new HttpBodyStream($body->pipe(
+                ZlibFilterStream::createDeflateCompressor(1)
+            ), null);
+        }
 
-        $compressor = ZlibFilterStream::createDeflateCompressor(1);
-
-        $in->pipe($compressor)->pipe($out);
-
-        return new HttpBodyStream($out, null);
+        return \RingCentral\Psr7\stream_for(
+            gzencode($body->getContents(), -1, FORCE_DEFLATE)
+        );
     }
 }
