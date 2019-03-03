c81c3ccd32199146af7f3f1dd4c3ee2d6b5bd5e6
Christoph Kluge <work@christoph-kluge.eu>
2019-03-03 13:05:36 +0100

    Adopted examples to work with the most current version of react/http

--- examples/01-server-with-compression.php
+++ examples/01-server-with-compression.php
@@ -2,7 +2,6 @@
 
 use Psr\Http\Message\ServerRequestInterface;
 use React\EventLoop\Factory;
-use React\Http\Io\MiddlewareRunner;
 use React\Http\Response;
 use React\Http\Server;
 use Sikei\React\Http\Middleware\ResponseCompressionMiddleware;
@@ -13,26 +12,21 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $loop = Factory::create();
 
-$server = new Server(new MiddlewareRunner([
+$server = new Server([
     new ResponseCompressionMiddleware([
         new CompressionGzipHandler(),
         new CompressionDeflateHandler(),
     ]),
-    function (ServerRequestInterface $request, callable $next) {
+    function (ServerRequestInterface $request) {
         return new Response(200, ['Content-Type' => 'application/json'], json_encode([
             'some' => 'nice',
             'json' => 'values',
         ]));
-    },
-]));
+    }
+]);
 
 $socket = new \React\Socket\Server(isset($argv[1]) ? $argv[1] : '0.0.0.0:0', $loop);
 $server->listen($socket);
-
-$server->on('error', function(Exception $error) {
-    echo get_class($error) . ' - ' . $error->getMessage() . PHP_EOL;
-    echo get_class($error->getPrevious()) . ' - ' . $error->getPrevious()->getMessage() . PHP_EOL;
-});
 
 echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . PHP_EOL;
 
